9add07a7fdb97c59846c01283a7977f6a9489a1c
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-07-20 22:08:12 -0300

    feat: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -3,16 +3,26 @@ import {
   AddAccountModel,
   AccountModel,
   Encrypter,
+  AddAccountRepository,
 } from "./db-add-account-protocols";
+
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter;
+  private readonly addAccountRepository: AddAccountRepository;
 
-  constructor(encrypter: Encrypter) {
+  constructor(
+    encrypter: Encrypter,
+    addAccountRepository: AddAccountRepository
+  ) {
     this.encrypter = encrypter;
+    this.addAccountRepository = addAccountRepository;
   }
 
-  async add(account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password);
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedpassword = await this.encrypter.encrypt(accountData.password);
+    await this.addAccountRepository.add(
+      Object.assign({}, accountData, { password: hashedpassword })
+    );
     return new Promise((resolve) => resolve(null));
   }
 }
