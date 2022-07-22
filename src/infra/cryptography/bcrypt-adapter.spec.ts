5b3158efee556dfa8e94cfb6cb1479f05fa0dd9b
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-07-21 21:23:39 -0300

    test: test bcrypt

---
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -0,0 +1,35 @@
+import bcrypt from "bcrypt";
+import { BcryptAdapter } from "./bcrypt-adapter";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
+
+const salt = 12;
+const makeSut = (): BcryptAdapter => {
+  return new BcryptAdapter(salt);
+};
+
+describe("Bcrypt Adapter", () => {
+  test("Should call bcrypt with correct value", async () => {
+    const sut = makeSut();
+    const hashSpy = jest.spyOn(bcrypt, "hash");
+    await sut.encrypt("any_value");
+    expect(hashSpy).toHaveBeenCalledWith("any_value", salt);
+  });
+
+  test("Should return a hash on success", async () => {
+    const sut = makeSut();
+    const hash = await sut.encrypt("any_value");
+    expect(hash).toBe("hash");
+  });
+
+  test("Should throw if bcrypt throws", async () => {
+    const sut = makeSut();
+    jest.spyOn(bcrypt, "hash").mockImplementationOnce(() => new Error());
+    const promisse = sut.encrypt("any_value");
+    await expect(promisse).rejects.toThrow();
+  });
+});
