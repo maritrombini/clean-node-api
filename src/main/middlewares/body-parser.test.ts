94f4a49abf9b2d13588abccc52096bbd00888b80
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-07-27 14:48:42 -0300

    feat: ensure requests parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import * as request from "supertest";
+import app from "../config/app";
+
+describe("Body Parser Middleware", () => {
+  test("Should parse body as json", async () => {
+    app.post("/test_body_parser", (req, res) => {
+      res.send(req.body);
+    });
+    await request(app)
+      .post("/test_body_parser")
+      .send({ name: "Mariana" })
+      .expect({ name: "Mariana" });
+  });
+});
