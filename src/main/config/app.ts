c7d2bbdb1c8ce8f1e6d15c10a70bb7924dd1688f
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-07-28 20:52:40 -0300

    refactor: connect to mongodb before integration tests

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,4 +1,5 @@
-import * as express from "express";
+import express from "express";
+// import * as express from "express";
 import setupMiddlewares from "./middlewares";
 import setUpRoutes from "./routes";
 
