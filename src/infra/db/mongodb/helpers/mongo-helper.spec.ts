eed332eaaddca68a800b12a39773d9f3401a0e85
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-08-01 21:02:01 -0300

    test: ensure mongodb reconnect if client is down

---
+++ src/infra/db/mongodb/helpers/mongo-helper.spec.ts
@@ -0,0 +1,17 @@
+import { MongoHelper as sut } from "./mongo-helper";
+
+describe("Mongo Helper", () => {
+  beforeAll(async () => {
+    await sut.connect(global.__MONGO_URI__);
+  });
+  afterAll(async () => {
+    await sut.disconnect();
+  });
+  test("Should reconnect if mongodb is down", async () => {
+    let accountCollection = await sut.getCollection("accounts");
+    expect(accountCollection).toBeTruthy();
+    await sut.disconnect();
+    accountCollection = await sut.getCollection("accounts");
+    expect(accountCollection).toBeTruthy();
+  });
+});
--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -11,10 +11,10 @@ describe("SignUp Routes", () => {
     await MongoHelper.disconnect();
   });
 
-  beforeEach(async () => {
-    const accountCollection = MongoHelper.getCollection("accounts");
-    await accountCollection.deleteMany({});
-  });
+  // beforeEach(async () => {
+  //   const accountCollection = await MongoHelper.getCollection("accounts");
+  //   await accountCollection.deleteMany({});
+  // });
   test("Should return an account on success", async () => {
     await request(app)
       .post("/api/signup")
