badccdb5dbbd415465154856f986654fdcee15f4
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-08-02 20:14:18 -0300

    refactor: move sut creation to a factory method

---
+++ src/main/decorators/log.spec.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse,
+} from "../../presentation/protocols";
+import { LogControllerDecorator } from "./log";
+
+interface SutTypes {
+  sut: LogControllerDecorator;
+  controllerStub: Controller;
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        statusCode: 200,
+        body: {
+          name: "Mari",
+          email: "mari@email.com",
+          password: "123",
+          passwordConfirmation: "123",
+        },
+      };
+      return new Promise((resolve) => resolve(httpResponse));
+    }
+  }
+  return new ControllerStub();
+};
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController();
+  const sut = new LogControllerDecorator(controllerStub);
+  return { sut, controllerStub };
+};
+
+describe("LogController Decorator", () => {
+  test("Should call controller handle", async () => {
+    const { sut, controllerStub } = makeSut();
+    const handleSpy = jest.spyOn(controllerStub, "handle");
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    await sut.handle(httpRequest);
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest);
+  });
+});
