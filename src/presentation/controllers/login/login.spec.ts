aa7a81530b22280ba37badf0c2f8f11cbff6f71f
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-08-05 13:07:58 -0300

    test: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -3,6 +3,7 @@ import {
   badRequest,
   serverError,
   unauthorized,
+  ok,
 } from "../../helpers/http-helper";
 import { InvalidParamError, MissingParamError } from "../../errors";
 import { EmailValidator, HttpRequest, Authentication } from "./login-protocols";
@@ -112,5 +113,10 @@ describe("Login Controller", () => {
       );
     const httpResponse = await sut.handle(makeFakeRequest());
     expect(httpResponse).toEqual(serverError(new Error()));
+  });
+  test("Should return 200 if valid credentials are provided", async () => {
+    const { sut, authenticationStub } = makeSut();
+    const httpResponse = await sut.handle(makeFakeRequest());
+    expect(httpResponse).toEqual(ok({ accessToken: "any_token" }));
   });
 });
