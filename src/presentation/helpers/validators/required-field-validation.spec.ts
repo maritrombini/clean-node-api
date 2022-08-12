a564bba7ac31ca13eff9369a11e55774073a038b
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-08-12 12:51:16 -0300

    test: ensure RequiredFieldValidation not return if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -2,9 +2,14 @@ import { MissingParamError } from "../../errors";
 import { RequiredFieldValidation } from "./required-field-validation";
 
 describe("RequiredField Validation", () => {
-  test("Should return a MissingParamError ir validation fails", () => {
+  test("Should return a MissingParamError if validation fails", () => {
     const sut = new RequiredFieldValidation("field");
     const error = sut.validate({ name: "any_name" });
     expect(error).toEqual(new MissingParamError("field"));
+  });
+  test("Should not return if validation succeeds", () => {
+    const sut = new RequiredFieldValidation("field");
+    const error = sut.validate({ field: "any_name" });
+    expect(error).toBeFalsy();
   });
 });
