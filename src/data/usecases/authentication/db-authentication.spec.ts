55b914828af47c98bcb9f311f1c8b2b76481a41a
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-08-16 19:24:37 -0300

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -51,4 +51,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makefakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return null if LoadAccountByEmailRepository with returns null', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'load').mockReturnValueOnce(null)
+    const accessToken = await sut.auth(makefakeAuthentication())
+    expect(accessToken).toBeNull()
+  })
 })
