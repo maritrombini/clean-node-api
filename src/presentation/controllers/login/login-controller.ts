871e1174e41efd4bf2739ec19fb97819c79a4389
Mariana Trombini <mariana.trombini@dasa.com.br>
2022-08-23 20:52:24 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login-controller.ts
+++ src/presentation/controllers/login/login-controller.ts
@@ -13,13 +13,9 @@ import {
 import { Validation } from "../../protocols/validation";
 
 export class LoginController implements Controller {
-  private readonly validation: Validation;
-  private readonly authentication: Authentication;
-
-  constructor(authentication: Authentication, validation: Validation) {
-    this.validation = validation;
-    this.authentication = authentication;
-  }
+  constructor(
+    private readonly authentication: Authentication, 
+    private readonly validation: Validation) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const error = this.validation.validate(httpRequest.body);
--- src/presentation/controllers/signup/signup-controller.ts
+++ src/presentation/controllers/signup/signup-controller.ts
@@ -8,13 +8,9 @@ import { badRequest, serverError, ok } from "../../helpers/http/http-helper";
 import { Validation } from "../../protocols/validation";
 
 export class SignUpController implements Controller {
-  private readonly addAccount: AddAccount;
-  private readonly validation: Validation;
-
-  constructor(addAccount: AddAccount, validation: Validation) {
-    this.addAccount = addAccount;
-    this.validation = validation;
-  }
+  constructor(
+    private readonly addAccount: AddAccount, 
+    private readonly validation: Validation) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
